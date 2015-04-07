4b7a3a84fdef7f2dcdd24178ae4c27c6551c7154
Phorn Sopheak <phornsopheak17@gmail.com>
2015-04-07 16:31:16 +0700

    finish log in / log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    def show
+
+  def show
     @user = User.find(params[:id])
   end
 
@@ -7,9 +8,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -24,4 +26,3 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'static_pages/home'
@@ -65,5 +67,8 @@ get 'help' =>'static_pages#help'
 get 'about' => 'static_pages#about'
 get  'contact' => 'static_pages#contact'
 get 'signup' => 'users#new'
+get 'login' => 'sessions#new'
+post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
 resources :users
 end
