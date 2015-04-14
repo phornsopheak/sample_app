54da0ee9f8429e4b9c65882b9a066409bbcaff69
Phorn Sopheak <phornsopheak17@gmail.com>
2015-04-14 14:16:35 +0700

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -71,4 +71,5 @@ get 'login' => 'sessions#new'
 post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
 resources :users
+resources :account_activations, only: [:edit]
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,19 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -10,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password:              "password",
                                             password_confirmation: "password" }
     end
-    assert_template 'users/show'
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
