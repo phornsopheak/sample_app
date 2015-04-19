2dded4dfa4224cf059ad7c83df65abc03ce9f886
Phorn Sopheak <phornsopheak17@gmail.com>
2015-04-19 21:11:27 +0700

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,7 +87,7 @@ class User < ActiveRecord::Base
   end
   # Returns a user's status feed.
   def feed
-     Mfollowing_ids = "SELECT followed_id FROM relationships
+     following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id)
